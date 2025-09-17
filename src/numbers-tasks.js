b4f73ea0eceddbb94e1f708531b48404b694cc25
donnyg <35265056+donnyg@users.noreply.github.com>
2025-09-17 16:49:27 +0300

    feat: implement `getFibonacciNumber` function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -247,8 +247,10 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  return index > 1
+    ? getFibonacciNumber(index - 2) + getFibonacciNumber(index - 1)
+    : index;
 }
 
 /**
